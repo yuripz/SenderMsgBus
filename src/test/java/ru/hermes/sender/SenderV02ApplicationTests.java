d0a2a5ae3324c7e002007fcce2bc770b663086a8
YuriPz <yuripz@gmail.com>
2022-09-11 01:55:39 +0300

    -

---
+++ src/test/java/ru/hermes/sender/SenderV02ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.hermes.sender;
+
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+
+//@SpringBootTest
+class SenderV02ApplicationTests {
+
+	//@Test
+	void contextLoads() {
+	}
+
+}
