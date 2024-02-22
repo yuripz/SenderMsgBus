e7eeb727b67c7db6584fb3bfdeca6c9a88e3d4c8
YuriPz <yuripz@gmail.com>
2024-02-22 17:48:38 +0300

    FIX postgresql 2 version 42.4.5: ostgresql:postgresql vulnerable to SQL Injection via line comment generation

--- src/main/java/net/plumbing/msgbus/SenderApplication.java
+++ src/main/java/net/plumbing/msgbus/SenderApplication.java
@@ -54,7 +54,7 @@ public class SenderApplication implements CommandLineRunner {
 	public TelegramProperties telegramProperties;
 
 	public static String propJDBC;
-	public static final String ApplicationName="*Sender* Application v.2.23.12.39";
+	public static final String ApplicationName="*Sender_BUS* v.4.02.22";
 	public static void main(String[] args) {
 		SpringApplication.run(SenderApplication.class, args);
 	}
