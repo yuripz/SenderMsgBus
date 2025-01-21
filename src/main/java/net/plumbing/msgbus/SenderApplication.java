70e1e4c843a16221d4d1abd18dcc2155c16db43e
Yurip <yuripz@gmail.com>
2025-01-21 22:18:34 +0300

    back-4-oracle

--- src/main/java/net/plumbing/msgbus/SenderApplication.java
+++ src/main/java/net/plumbing/msgbus/SenderApplication.java
@@ -55,7 +55,7 @@ public class SenderApplication implements CommandLineRunner {
 	public static String propJDBC;
 	public static String propExtJDBC;
 	public static String firstInfoStreamId;
-	public static final String ApplicationName="*Sender_BUS* v.5.01.15p";
+	public static final String ApplicationName="*Sender_BUS* v.5.01.15o";
 	public static void main(String[] args) {
 		SpringApplication.run(SenderApplication.class, args);
 	}
