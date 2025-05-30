36e82b4b6a9c95526fc75ac6ca5f5fd98cc3a598
YuriPz <yuripz@gmail.com>
2025-05-30 21:45:18 +0300

    update to Saxon-12.6

--- src/main/java/net/plumbing/msgbus/SenderApplication.java
+++ src/main/java/net/plumbing/msgbus/SenderApplication.java
@@ -55,7 +55,7 @@ public class SenderApplication implements CommandLineRunner {
 	public static String propJDBC;
 	public static String propExtJDBC;
 	public static String firstInfoStreamId;
-	public static final String ApplicationName="*Sender_BUS* v.5.05.07";
+	public static final String ApplicationName="*Sender_BUS* v.5.05.30";
 	public static void main(String[] args) {
 		SpringApplication.run(SenderApplication.class, args);
 	}
